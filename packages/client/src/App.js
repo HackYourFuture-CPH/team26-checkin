bade3e11751739317d014dd8f8d0c911b7a997a3
rumana-aafrin <rumanaraafi@gmail.com>
2024-04-02 12:32:17 +0200

    Landing page frontend

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from './component/Login';
-// import { LandingPage } from './containers/LandingPage/LandingPage.Container';
-// import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
+import LandingPageContainer from './containers/LandingPage/LandingPage.Container';
+import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
 
 function App() {
   return (
     <div className="app">
-      <Login />
-      Hello
-      {/* <Router>
+      <Router>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<LandingPageContainer />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </Router> */}
+      </Router>
     </div>
   );
 }
--- packages/client/src/containers/LandingPage/LandingPage.Container.js
+++ packages/client/src/containers/LandingPage/LandingPage.Container.js
@@ -1,25 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import { apiURL } from '../../apiURL';
+// import React, { useEffect, useState } from 'react';
+// import { apiURL } from '../../apiURL';
+// import './LandingPage.Style.css';
+
+// export const LandingPage = () => {
+//   const [exampleResources, setExampleResources] = useState([]);
+//   useEffect(() => {
+//     async function fetchExampleResources() {
+//       const response = await fetch(`${apiURL()}/exampleResources`);
+//       const examples = await response.json();
+//       setExampleResources(examples);
+//     }
+
+//     fetchExampleResources();
+//   }, []);
+
+//   return (
+//     <div className="landing-page-container">
+//       <span>Landing Page</span>
+//       {exampleResources.map((example) => (
+//         <div key={example.id}>{example.title}</div>
+//       ))}
+//     </div>
+//   );
+// };
+
+import React from 'react';
+import Testimonials from '../../components/Login/Testimonials';
+import logo from './logo.svg';
+import { Typography, TextField, Button } from '@mui/material';
+import styled from '@emotion/styled';
 import './LandingPage.Style.css';
+// Import necessary components from Material-UI
 
-export const LandingPage = () => {
-  const [exampleResources, setExampleResources] = useState([]);
-  useEffect(() => {
-    async function fetchExampleResources() {
-      const response = await fetch(`${apiURL()}/exampleResources`);
-      const examples = await response.json();
-      setExampleResources(examples);
-    }
+const CustomButton = styled(Button)({
+  padding: '14px 20px',
+  backgroundColor: 'var(--primary-color)',
+  color: 'white',
+});
 
-    fetchExampleResources();
-  }, []);
-
+const LandingPageContainer = () => {
   return (
-    <div className="landing-page-container">
-      <span>Landing Page</span>
-      {exampleResources.map((example) => (
-        <div key={example.id}>{example.title}</div>
-      ))}
+    <div className="login-page">
+      <div className="testimonial-container">
+        <Typography className="text-field" variant="h4" color="white">
+          Don’t forget to invite teammates!
+        </Typography>
+        <div className="testimonial-section">
+          <div className="testimonial-cards">
+            <Testimonials /> {/* Render the Testimonials component */}
+          </div>
+        </div>
+      </div>
+
+      <div className="login-container">
+        <img className="logo" src={logo} alt="Logo" />
+        <div className="login-details">
+          <Typography variant="h5" fontWeight="bold">
+            Log in to Teams Check-in
+          </Typography>
+          <div className="login-action">
+            <div className="login-text">
+              <Typography className="text-field" variant="body1">
+                Enter your team code
+              </Typography>
+              <TextField
+                className="typography"
+                label="Your code"
+                type="password"
+                variant="outlined"
+                size="small"
+                required
+              />
+            </div>
+            <CustomButton className="button" variant="contained" fullWidth>
+              Login
+            </CustomButton>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
+export default LandingPageContainer;
