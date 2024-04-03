510007882442b9c7d11dae46b261d5eebb6a1419
rumana-aafrin <rumanaraafi@gmail.com>
2024-04-03 13:38:51 +0200

    Merging landing page frontend

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPageContainer from './containers/LandingPage/LandingPage.Container';
+// import {LandingPageContainer} from './containers/LandingPage/LandingPage.Container';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
 
 function App() {
