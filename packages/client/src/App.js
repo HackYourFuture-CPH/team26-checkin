8523cb5593c8f03aed618516964a00b33277fc49
Yağmur Nielsen <yagmureva4@gmail.com>
2024-04-04 23:13:44 +0200

    bugs fixed

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
 import { LandingPage } from './containers/LandingPage/Landing';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound';
 import { Dashboard } from './containers/LandingPage/Dashboard';
-=======
-
-import LandingPageContainer from './containers/LandingPage/LandingPage.Container';
-import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
->>>>>>> main
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
-=======
-          <Route path="/" element={<LandingPageContainer />} />
->>>>>>> main
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
