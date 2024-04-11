b12eb8e2e1d03013a1fce677f1e70755d8d820c1
rumana-aafrin <rumanaraafi@gmail.com>
2024-04-11 15:30:31 +0200

    Dashboard frontend navigation bar

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,31 +1,9 @@
-/* eslint-disable */
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import './App.css';
-// import { LandingPageContainer } from './containers/LandingPage/LandingPage';
-// import { Dashboard } from './containers/LandingPage/Dashboard';
-// import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
-
-// function App() {
-//   return (
-//     <div className="app">
-//       <Router>
-//         <Routes>
-//           <Route path="/" element={<LandingPageContainer />} />
-//           <Route path="/dashboard" element={<Dashboard />} />
-//           <Route path="*" element={<PageNotFound />} />
-//         </Routes>
-//       </Router>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
+// /* eslint-disable */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './containers/Dashboard/Dashboard';
+// import { LandingPageContainer } from './containers/LandingPage/LandingPage';
+import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
 
 
 function App() {
@@ -33,7 +11,9 @@ function App() {
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+         {/* <Route path="/" element={<LandingPageContainer />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
     </div>
