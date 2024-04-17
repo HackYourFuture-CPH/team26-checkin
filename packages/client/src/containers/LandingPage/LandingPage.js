2c394f71bae5bc7b405e113b4051009a3c151a96
ddobby94 <dobisz_david@windowslive.com>
2024-04-17 14:54:18 +0200

    Update packages/client/src/containers/LandingPage/LandingPage.js

--- packages/client/src/containers/LandingPage/LandingPage.js
+++ packages/client/src/containers/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { apiURL } from '../../apiURL';
 import Testimonials from '../../components/Login/Testimonials';
 import { useNavigate } from 'react-router-dom';
