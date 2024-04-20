86204dde129ba159815db290290c82c75dd40381
Yağmur Nielsen <yagmureva4@gmail.com>
2024-04-20 14:26:29 +0200

    fixed

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './containers/Dashboard/Dashboard';
-// TODO: put back comment after the login flow is down
-// import { LandingPageContainer } from './containers/LandingPage/LandingPage';
-import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
-import { CheckinQuestions } from './containers/LandingPage/CheckinQuestionsPage/CheckinQuestions';
+import { LandingPageContainer } from './containers/LandingPage/LandingPage';
+import { PageNotFound } from './containers/PageNotFound/PageNotFound';
+import { CheckinQuestions } from './containers/Dashboard/CheckinQuestionsPage/CheckinQuestions';
 import { TeamIdContextProvider } from './hooks/contextHook';
 
 function App() {
@@ -13,8 +12,7 @@ function App() {
       <TeamIdContextProvider>
         <Router>
           <Routes>
-            {/* TODO: put back comment after the login flow is down */}
-            {/* <Route path="/" element={<LandingPageContainer />} /> */}
+            <Route path="/" element={<LandingPageContainer />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/questions" element={<CheckinQuestions />} />
             <Route path="*" element={<PageNotFound />} />
