ba3a8dd0183fc87ea4ea4a1a8aafb5f1c37f2500
rumana-aafrin <rumanaraafi@gmail.com>
2024-04-25 21:38:32 +0200

    Implemented report page

--- packages/client/src/components/Dashboard/TeamMemberListItem.jsx
+++ packages/client/src/components/Dashboard/TeamMemberListItem.jsx
@@ -5,7 +5,6 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 //import Dashboard from '../../containers/Dashboard/Dashboard';
 
-
 const TeamMemberListItem = ({
   member,
   handleEditMember,
